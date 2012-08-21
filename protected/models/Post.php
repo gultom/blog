27ebb2dfb19913eeb9aea71762af9112e3d82ce8
Charles Gultom <charles@w3magz.com>
2012-08-21 13:03:35 +0700

    modified rules method

--- protected/models/Post.php
+++ protected/models/Post.php
@@ -45,13 +45,15 @@ class Post extends CActiveRecord
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
-			array('title, content, status, author_id', 'required'),
-			array('status, create_time, update_time, author_id', 'numerical', 'integerOnly'=>true),
+			array('title, content, status', 'required'),
 			array('title', 'length', 'max'=>128),
-			array('tags', 'safe'),
+            array('status', 'in', 'range' => array(1, 2, 3)), /** status info : 1 = draft, 2 = published, 3 = archived **/
+			array('tags', 'match', 'pattern' => '/^[\w\s,]+$/',
+                                   'message' => 'Tags can only contain word characters'),
+            array('tags', 'normalizeTags'),
 			// The following rule is used by search().
 			// Please remove those attributes that should not be searched.
-			array('id, title, content, tags, status, create_time, update_time, author_id', 'safe', 'on'=>'search'),
+			array('title, status', 'safe', 'on'=>'search'),
 		);
 	}
 
